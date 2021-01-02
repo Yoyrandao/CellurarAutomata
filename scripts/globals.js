3522b58149483fdf06221faa8d00848748cb9b4d
Gubanov Ilya <yoyrandao@gmail.com>
2021-01-02 19:23:17 +0300

    base version

---
+++ scripts/globals.js
@@ -0,0 +1,10 @@
+export const CANVAS_WIDTH = 90;
+export const CANVAS_HEIGHT = 90;
+
+export const RANDOM_PERCENTAGE = 0.25;
+
+export const X_SCALE = 10;
+export const Y_SCALE = 10;
+
+export const MAKE_ALIVE_CONDITION_DEFAULT = [3];
+export const NO_KILL_CONDITION_DEFAULT = [2,3];
